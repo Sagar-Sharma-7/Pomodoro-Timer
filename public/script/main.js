981f22d61cf37090120827e1d978fb26cf05857b
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-08-19 17:10:34 +0530

    filler scripting

--- public/script/main.js
+++ public/script/main.js
@@ -11,6 +11,8 @@ const orbit1 = document.querySelector(".orbit1");
 const orbit2 = document.querySelector(".orbit2");
 const orbit3 = document.querySelector(".orbit3");
 const orbit4 = document.querySelector(".orbit4");
+const filler = document.querySelector("#filler");
+const time_spent = document.querySelector("#time_spent");
 
 t25.value = 25;
 t30.value = 30;
@@ -18,10 +20,12 @@ t35.value = 35;
 t40.value = 40;
 
 let timer_min;
+
 boxes.forEach(box => {
     const min_v = box.value;
     box.addEventListener("click", () => {
         orbit4.style.animationName = "rotate4";
+        filler.style.display = "block";
         $("#timer_slot").slideUp();
         min.innerHTML = min_v;
         timer_min = min_v;
@@ -41,15 +45,17 @@ start.addEventListener("click", () => {
     const date = new Date(milli);
     const s = date.getSeconds();
     let i = timer_min -1;
+    z = 0;
     const timer = setInterval(() =>{
         const now = new Date();
         let s_n = now.getSeconds();
         let sLeft = s - s_n;
+        time_spent.style.width = `${z}%`;
         if(i < 10){
             min.innerHTML = "0" + i;
         }else{
             min.innerHTML = i;
-        }
+        }   
         if(sLeft == 0){
             i -= 1;
         }
@@ -66,7 +72,7 @@ start.addEventListener("click", () => {
                 sec.innerHTML = 60 + sLeft;
             }
         };
-
+        z += 100/(timer_min *60);
     }, 1000);
 
 });
